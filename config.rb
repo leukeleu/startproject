767a80388907728414ac7de4cc2f793dc10036c2
Niels van Dijk <nvandijk@leukeleu.nl>
2011-11-01 10:16:33 +0100

    added compass to the project added git hook pre-commit updated template with compass

---
+++ config.rb
@@ -0,0 +1,13 @@
+http_path = "/"
+css_dir = "pages/static/css"
+sass_dir = "sass"
+images_dir = "pages/static/images"
+javascripts_dir = "pages/static/js"
+
+output_style = :expanded # or :nested or :compact or :compressed
+relative_assets = true
+line_comments = true
+
+sass_options = {
+    :debug_info => true
+}
