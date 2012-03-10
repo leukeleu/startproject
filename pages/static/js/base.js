014b8a55fcdcec63235563d4e1378a08b26e8755
Niels van Dijk <nvandijk@leukeleu.nl>
2012-03-10 20:22:45 +0100

    converted tabs to spaces in scss, js and html files

--- pages/static/js/base.js
+++ pages/static/js/base.js
@@ -4,26 +4,26 @@ var DFSP = {};
 
 DFSP.init = function() {
 
-	// call base methods
-	DFSP.external_links();
-	DFSP.download_links();
+    // call base methods
+    DFSP.external_links();
+    DFSP.download_links();
 };
 
 DFSP.external_links = function() {
-	// open external links in new window
-	$('a[href*="http://"]:not([href*="' + location.hostname + '"]), a[href*="https://"]:not([href*="' + location.hostname + '"])')
-		.addClass('external')
-		.attr('target', '_blank')
-		.attr('title', 'Open in nieuw venster');
+    // open external links in new window
+    $('a[href*="http://"]:not([href*="' + location.hostname + '"]), a[href*="https://"]:not([href*="' + location.hostname + '"])')
+        .addClass('external')
+        .attr('target', '_blank')
+        .attr('title', 'Open in nieuw venster');
 };
 
 DFSP.download_links = function() {
-	// open download links in new window
-	$('a[href$=".pdf"], a[href$=".doc"], a[href$=".docx"], a[href$=".xls"], a[href$=".xlsx"], a[href$=".zip"], a[href$=".gz"]')
-		.addClass('download')
-		.attr('target', '_blank');
+    // open download links in new window
+    $('a[href$=".pdf"], a[href$=".doc"], a[href$=".docx"], a[href$=".xls"], a[href$=".xlsx"], a[href$=".zip"], a[href$=".gz"]')
+        .addClass('download')
+        .attr('target', '_blank');
 };
 
 $(function() {
-	DFSP.init();
+    DFSP.init();
 });
