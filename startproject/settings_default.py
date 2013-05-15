47cec32839c3525f6df42d264d28802b4a3fa141
Bram Simons <bsimons@leukeleu.nl>
2013-05-15 14:46:02 +0200

    PEP 8

--- startproject/settings_default.py
+++ startproject/settings_default.py
@@ -92,7 +92,7 @@ SECRET_KEY = ''
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    # 'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = DEFAULT_SETTINGS.MIDDLEWARE_CLASSES + (
